a459a8e80396ea5e16a29235b3184546ad6a7653
Leon Lambert <shi406406@qq.com>
2021-07-14 17:53:37 +0800

    build: 更新app icon

--- src/main/kotlin/me/leon/MyApp.kt
+++ src/main/kotlin/me/leon/MyApp.kt
@@ -8,6 +8,6 @@ import tornadofx.*
 class MyApp: App(Home::class, Styles::class) {
     override fun start(stage: Stage) {
         super.start(stage)
-        stage.icons.add(Image(resources.stream("/tb.jpg")))
+        stage.icons.add(Image(resources.stream("/tb.png")))
     }
 }
