0195f8eaab0d6535ef88546cc8f1fe01105abbc1
Leon Lambert <shi406406@qq.com>
2021-09-02 10:01:30 +0800

    fix:base58/base58check部分字符解码错误

--- src/main/kotlin/me/leon/base/BaseN.kt
+++ src/main/kotlin/me/leon/base/BaseN.kt
@@ -9,7 +9,7 @@ fun String.baseNEncode(radix: Int = 58, maps: String = BASE_58_DICT): String {
 }
 
 fun ByteArray.baseNEncode(radix: Int = 58, maps: String = BASE_58_DICT): String {
-    var bigInteger = BigInteger(this)
+    var bigInteger = BigInteger(1, this)
     var remainder: Int
     val sb = StringBuilder()
     while (bigInteger != BigInteger.ZERO) {
@@ -22,13 +22,46 @@ fun ByteArray.baseNEncode(radix: Int = 58, maps: String = BASE_58_DICT): String 
     return sb.reversed().toString()
 }
 
-fun String.baseNDecode(radix: Int = 58, maps: String = BASE_58_DICT) =
-    toCharArray()
-        .mapIndexed { index, c -> length - index - 1 to maps.indexOf(c) }
-        .fold(0.toBigInteger()) { acc, pair ->
-            acc.add(pair.second.toBigInteger().multiply(radix.toBigInteger().pow(pair.first)))
+fun String.baseNDecode(radix: Int = 58, maps: String = BASE_58_DICT): ByteArray {
+    if (this.isEmpty()) {
+        return ByteArray(0)
+    }
+    var intData = BigInteger.ZERO
+    var leadingZeros = 0
+    for (i in this.indices) {
+        val current = this[i]
+        val digit = maps.indexOf(current)
+        require(digit != -1) {
+            String.format("Invalid Base58 character `%c` at position %d", current, i)
         }
-        .toByteArray()
+        intData = intData.multiply(radix.toBigInteger()).add(BigInteger.valueOf(digit.toLong()))
+    }
+
+    for (element in this) {
+        if (element == '1') {
+            leadingZeros++
+        } else {
+            break
+        }
+    }
+    val bytesData: ByteArray =
+        if (intData == BigInteger.ZERO) {
+            ByteArray(0)
+        } else {
+            intData.toByteArray()
+        }
+
+    val stripSignByte = bytesData.size > 1 && bytesData[0].toInt() == 0 && bytesData[1] < 0
+    val decoded = ByteArray(bytesData.size - (if (stripSignByte) 1 else 0) + leadingZeros)
+    System.arraycopy(
+        bytesData,
+        if (stripSignByte) 1 else 0,
+        decoded,
+        leadingZeros,
+        decoded.size - leadingZeros
+    )
+    return decoded
+}
 
 fun String.baseNDecode2String(radix: Int = 58, maps: String = BASE_58_DICT) =
     String(baseNDecode(radix, maps))
