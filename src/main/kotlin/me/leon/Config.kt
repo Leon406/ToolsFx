e90fbc717d0b842f0cafd42c8cf986fb6c64c73c
Leon <shi406406@qq.com>
2021-09-09 22:21:57 +0800

    build: change release version to 1.7.0

--- src/main/kotlin/me/leon/Config.kt
+++ src/main/kotlin/me/leon/Config.kt
@@ -1,6 +1,6 @@
 package me.leon
 
-const val VERSION = "1.6.1"
+const val VERSION = "1.7.0"
 const val REPO_URL = "https://github.com/Leon406/ToolsFx"
 const val PJ52_URL = "https://www.52pojie.cn/thread-1501153-1-1.html"
 const val LAN_ZOU_DOWNLOAD_URL = "https://leon.lanzoui.com/b0d9av2kb"
