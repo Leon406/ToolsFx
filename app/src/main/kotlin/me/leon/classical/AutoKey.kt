ba67fdcde093456791d3a81631ae2ebee0fe5031
Leon <shi406406@qq.com>
2021-09-25 17:18:21 +0800

    fix: autoKey keyword小写加密不一致

--- app/src/main/kotlin/me/leon/classical/AutoKey.kt
+++ app/src/main/kotlin/me/leon/classical/AutoKey.kt
@@ -3,25 +3,27 @@ package me.leon.classical
 import me.leon.ext.sliceList
 
 fun String.autoKey(keyword: String): String {
+    val key = keyword.uppercase()
     val stripText = this.replace("\\s".toRegex(), "")
     val splits = split("\\s+".toRegex()).map { it.length }.also { println(it) }
     return stripText
-        .virgeneneEncode(keyword + stripText.also { println(it) })
+        .virgeneneEncode(key + stripText.also { println(it) })
         .also { println(it) }
         .sliceList(splits)
 }
 
 fun String.autoKeyDecrypt(keyword: String): String {
+    val key = keyword.uppercase()
     val splits = split("\\s".toRegex()).map { it.length }.also { println(it) }
     val stripText = this.replace("\\s".toRegex(), "")
-    val keyBuilder = StringBuilder(stripText.length + keyword.length)
-    keyBuilder.append(keyword)
-    while (keyBuilder.length < stripText.length + keyword.length) {
+    val keyBuilder = StringBuilder(stripText.length + key.length)
+    keyBuilder.append(key)
+    while (keyBuilder.length < stripText.length + key.length) {
         val substring =
             stripText
                 .virgeneneDecode(keyBuilder.toString(), keyBuilder.length)
                 .substring(
-                    keyBuilder.length - keyword.length,
+                    keyBuilder.length - key.length,
                     keyBuilder.length.takeIf { it < stripText.length } ?: stripText.length
                 )
         keyBuilder.append(substring)
