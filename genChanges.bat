80d37fd3d59b5ee2beae6f51e6c6d6b885a211f2
Leon <deadogone@gmail.com>
2021-12-14 23:41:51 +0800

    doc: update changelog.md

--- genChanges.bat
+++ genChanges.bat
@@ -1,7 +1,7 @@
 @echo off
 echo # %2  >> changelog.md
 echo ## feature:  >> changelog.md
-git log %1..%2 --pretty="- %%s" |findstr feat: >> changelog.md
+git log %1..%2 --pretty="- %%s" |findstr feat>> changelog.md
 echo ## bug fix:  >>changelog.md
-git log %1..%2 --pretty="- %%s" |findstr  fix:  >> changelog.md
+git log %1..%2 --pretty="- %%s" |findstr  fix>> changelog.md
 echo generate complete
