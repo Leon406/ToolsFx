047ed0a321a701054de761bdce7288fa1948d45d
Leon Lambert <deadogone@gmail.com>
2021-12-28 17:37:38 +0800

    build(apipost): bump spotless version to 6.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
     kotlin("jvm") version "1.6.10"
-    id("com.diffplug.spotless") version "6.0.0"
+    id("com.diffplug.spotless") version "6.1.0"
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
     id("org.openjfx.javafxplugin") version "0.0.10"
 }
