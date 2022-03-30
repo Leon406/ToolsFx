c00cc8f3e44cf4c58537980610f73dc5b4ff0d9c
Leon <deadogone@gmail.com>
2022-03-30 23:45:33 +0800

    build(app): start 1.12.2.beta07

--- app/src/main/kotlin/me/leon/Config.kt
+++ app/src/main/kotlin/me/leon/Config.kt
@@ -2,7 +2,7 @@ package me.leon
 
 import java.io.File
 
-const val VERSION = "1.12.2.beta06"
+const val VERSION = "1.12.2.beta07"
 const val BUILD_DATE = "2022-03-30"
 const val REPO_URL = "https://github.com/Leon406/ToolsFx"
 const val REPO_ISSUE = "https://github.com/Leon406/ToolsFx/issues/new"
