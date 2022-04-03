ed96f7fa5565f3150c750854e4bf5b1410ab9984
Leon <deadogone@gmail.com>
2022-04-03 22:03:44 +0800

    feat(app): qrcode支持一图多码识别

--- app/src/main/kotlin/me/leon/ext/fx/Zxing.kt
+++ app/src/main/kotlin/me/leon/ext/fx/Zxing.kt
@@ -4,6 +4,7 @@ import com.google.zxing.*
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource
 import com.google.zxing.client.j2se.MatrixToImageWriter
 import com.google.zxing.common.HybridBinarizer
+import com.google.zxing.multi.GenericMultipleBarcodeReader
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 import java.awt.image.BufferedImage
 import java.io.File
@@ -23,13 +24,20 @@ fun BufferedImage.qrReader(): String {
     // 定义二维码参数
     val hints = Hashtable<DecodeHintType, Any>()
     hints[DecodeHintType.CHARACTER_SET] = "utf-8"
-    val result = formatReader.decode(binaryBitmap, hints)
-    // 输出相关的二维码信息
-    println("解析结果：$result")
-    println("二维码格式类型：" + result.barcodeFormat)
-    println("二维码文本内容：" + result.text)
+    val results = GenericMultipleBarcodeReader(formatReader).decodeMultiple(binaryBitmap)
+        .sortedBy { it.resultPoints.first().x }
+        .fold(StringBuilder()){
+            acc, result ->
+            println(result.resultPoints.map { "${it.x} ${it.y}" })
+            println("解析结果：$result")
+            println("二维码格式类型：" + result.barcodeFormat)
+            println("二维码文本内容：" + result.text)
+            if (acc.isNotEmpty()) acc.appendLine()
+            acc.append(result.text)
+        }.toString()
+
     this.flush()
-    return result.text
+    return results
 }
 
 fun String.createQR(
