9efe9f7a8dde56472e3abda4d3ee64e041874bf0
Leon Lambert <deadogone@gmail.com>
2022-04-12 18:53:34 +0800

    test(app): ans1 解析

--- app/src/test/kotlin/me/leon/asymmetric/CertificationParse.kt
+++ app/src/test/kotlin/me/leon/asymmetric/CertificationParse.kt
@@ -3,8 +3,10 @@ package me.leon.asymmetric
 import java.io.File
 import java.security.Security
 import me.leon.TEST_DATA_DIR
+import me.leon.encode.base.base64Decode
 import me.leon.ext.crypto.parsePublicKeyFromCerFile
 import org.bouncycastle.asn1.ASN1InputStream
+import org.bouncycastle.asn1.ASN1Sequence
 import org.bouncycastle.asn1.util.ASN1Dump
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.junit.Test
@@ -20,6 +22,9 @@ class CertificationParse {
     @Test
     fun ans1() {
         ASN1InputStream(File(TEST_DATA_DIR, "signature/SM2.cer").inputStream()).use {
+            println(ASN1Dump.dumpAsString(it.readObject(), true))
+        }
+        ASN1InputStream(File(TEST_DATA_DIR, "signature/p7.txt").readText().base64Decode()).use {
             println(ASN1Dump.dumpAsString(it.readObject(), true))
         }
     }
