2080b51b200b202f08947de857684e00ca00a4fa
Leon <deadogone@gmail.com>
2022-06-20 20:49:30 +0800

    build(app): update Config.kt

--- app/src/main/kotlin/me/leon/Config.kt
+++ app/src/main/kotlin/me/leon/Config.kt
@@ -3,7 +3,7 @@ package me.leon
 import java.io.File
 
 const val VERSION = "1.13.4.beta"
-const val BUILD_DATE = "2022-06-18"
+const val BUILD_DATE = "2022-06-20"
 const val REPO_URL = "https://github.com/Leon406/ToolsFx"
 const val REPO_ISSUE = "https://github.com/Leon406/ToolsFx/issues/new"
 const val PJ52_URL = "https://www.52pojie.cn/thread-1501153-1-1.html"
@@ -15,6 +15,6 @@ const val CHECK_UPDATE_URL2 = "https://gitee.com/LeonShih/ToolsFx/raw/main/updat
 const val DEV_UPDATE_URL =
     "https://ghproxy.com/https://raw.githubusercontent.com/Leon406/ToolsFx/dev/update.json"
 const val DEV_UPDATE_URL2 = "https://raw.githubusercontent.com/Leon406/ToolsFx/dev/update.json"
-const val LICENSE = "https://cdn.staticaly.com/gh/Leon406/ToolsFx/main/LICENSE"
+const val LICENSE = "https://raw.githubusercontent.com/Leon406/ToolsFx/main/LICENSE"
 val APP_ROOT: String = File("").absolutePath
 val DICT_DIR: String = "$APP_ROOT/dict"
