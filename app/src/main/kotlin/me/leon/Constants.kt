83d27a378535b5a714c9e387707d83ca02c133be
Leon Lambert <deadogone@gmail.com>
2022-06-27 18:30:35 +0800

    fix(app): 去除NON_PRINTABLE换行符

--- app/src/main/kotlin/me/leon/Constants.kt
+++ app/src/main/kotlin/me/leon/Constants.kt
@@ -6,5 +6,5 @@ val CHARSETS = listOf("UTF-8", "ISO8859-1", "GBK", "GB2312", "GB18030")
 const val P1 = "p1"
 const val P2 = "p2"
 const val DEBUG = false
-val REG_NON_PRINTABLE = "[\u0000-\u001F]|解码错误:|�".toRegex()
+val REG_NON_PRINTABLE = "[\u0000-\u0009\u000B\u000C\u000E-\u001F]|解码错误:|�".toRegex()
 const val EXCEPTION_NON_PRINTABLE = "解码错误: contains non printable char!maybe wrong encoding!"
