abad0e4ce096548d0082eca1d2dff5aee0c830af
Leon Lambert <deadogone@gmail.com>
2022-07-06 17:51:50 +0800

    feat(app): 内置浏览器支持websocket, ws wss

--- app/src/main/kotlin/me/leon/ToolsApp.kt
+++ app/src/main/kotlin/me/leon/ToolsApp.kt
@@ -1,9 +1,12 @@
 package me.leon
 
 import java.io.File
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import java.util.Locale
 import java.util.Properties
 import javafx.scene.image.Image
+import javax.net.ssl.*
 import me.leon.ext.fx.Prefs
 import me.leon.view.Home
 import tornadofx.*
@@ -15,6 +18,36 @@ class ToolsApp : App(Home::class, Styles::class) {
         FX.locale = if (Prefs.language == "zh") Locale.CHINESE else Locale.ENGLISH
         addStageIcon(Image(resources.stream("/img/tb.png")))
         initConfig()
+        fixSsl()
+    }
+
+    private fun fixSsl() {
+        val trustManagers =
+            arrayOf(
+                object : X509TrustManager {
+                    override fun checkClientTrusted(
+                        chain: Array<out X509Certificate>?,
+                        authType: String?
+                    ) {
+                        // nop
+                    }
+
+                    override fun checkServerTrusted(
+                        chain: Array<out X509Certificate>?,
+                        authType: String?
+                    ) {
+                        // nop
+                    }
+
+                    override fun getAcceptedIssuers(): Array<X509Certificate>? {
+                        return null
+                    }
+                }
+            )
+        val sc = SSLContext.getInstance("TLSv1.2")
+        sc.init(null, trustManagers, SecureRandom())
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.socketFactory)
+        HttpsURLConnection.setDefaultHostnameVerifier { _, _ -> true }
     }
 
     private fun initConfig() {
