8f20bd02e7ecfd52f70d9cd1214ed1a97ed59608
Leon Lambert <deadogone@gmail.com>
2022-07-21 14:34:06 +0800

    build: bump jfx to 18.0.2 & remove hutool related code

--- app/src/test/kotlin/me/leon/asymmetric/Sm2Test.kt
+++ app/src/test/kotlin/me/leon/asymmetric/Sm2Test.kt
@@ -1,35 +1,11 @@
 package me.leon.asymmetric
 
-import cn.hutool.core.util.StrUtil
-import cn.hutool.crypto.SecureUtil
-import cn.hutool.crypto.SmUtil
-import cn.hutool.crypto.asymmetric.KeyType
 import kotlin.test.assertEquals
-import me.leon.encode.base.base64
 import me.leon.ext.crypto.allCurves
 import me.leon.ext.crypto.curveMultiply
 import org.junit.Test
 
 class Sm2Test {
-
-    // for check result
-    @Test
-    fun hutool() {
-        val text = "我是一段测试aaaa"
-
-        val pair = SecureUtil.generateKeyPair("SM2")
-        val privateKey = pair.private.encoded
-        val publicKey = pair.public.encoded
-
-        println(privateKey.base64())
-        println(publicKey.base64())
-
-        val sm2 = SmUtil.sm2(privateKey, publicKey)
-        val encryptStr = sm2.encryptBcd(text, KeyType.PublicKey)
-        println(encryptStr)
-        val decryptStr = StrUtil.utf8Str(sm2.decryptFromBcd(encryptStr, KeyType.PrivateKey))
-        println(decryptStr)
-    }
 
     @Test
     fun dotTimes() {
