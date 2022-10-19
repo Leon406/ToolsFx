af1963e918b6fce385d56324bed540686390df77
Leon Lambert <deadogone@gmail.com>
2022-10-19 10:20:36 +0800

    test(app): ignore ocr test

--- app/src/test/kotlin/me/leon/Ocr.kt
+++ app/src/test/kotlin/me/leon/Ocr.kt
@@ -1,26 +1,31 @@
 package me.leon
 
 import java.io.File
+import kotlin.test.*
 import me.leon.ext.ocr.BaiduOcr
 import me.leon.ext.toBase64
 import org.junit.Test
 
+@Ignore
 class Ocr {
 
     @Test
     fun urlOcr() {
         val data = "https://wx1.sinaimg.cn/mw2000/7736d59fly1gzpm3yc7m6j20j80ip755.jpg"
-        println(BaiduOcr.ocr(data))
+        assertTrue(BaiduOcr.ocr(data).contains("你们别看我"))
     }
 
     @Test
     fun base64Ocr() {
-        println(BaiduOcr.ocrBase64(File(TEST_OCR_DIR, "ocrbase64.txt").readText()))
+        assertTrue(
+            BaiduOcr.ocrBase64(File(TEST_OCR_DIR, "ocrbase64.txt").readText())
+                .contains("fromJson(BaiduOcrBean class.java)BaiduOcrBean")
+        )
     }
 
     @Test
     fun fileOcr() {
-        println(BaiduOcr.ocrBase64(File(TEST_OCR_DIR, "ocr.jpg").toBase64()))
+        assertTrue(BaiduOcr.ocrBase64(File(TEST_OCR_DIR, "ocr.jpg").toBase64()).contains("你们别看我平时"))
     }
 
     companion object {
