d883faece6ccd9c8cd8534857083a3426723788b
Leon Lambert <deadogone@gmail.com>
2022-11-15 18:37:46 +0800

    fix(app): 负数 circleIndex 错误

--- app/src/main/kotlin/me/leon/ext/math/Int.kt
+++ app/src/main/kotlin/me/leon/ext/math/Int.kt
@@ -2,4 +2,4 @@ package me.leon.ext.math
 
 /** support shift */
 fun Int.circleIndex(len: Int = 26, shift: Int = 0) =
-    with(this + shift) { if (this < 0) this % len + len else this % len }
+    with(this + shift) { if (this < 0) (this % len + len) else this } % len
