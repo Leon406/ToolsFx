5d47013a4105da75bd9c3573e12beaf4cf125d08
Leon406 <deadogone@gmail.com>
2022-11-27 11:01:55 +0800

    refactor(app): fracMorse 复用morse字典

--- app/src/main/kotlin/me/leon/ctf/FracMorse.kt
+++ app/src/main/kotlin/me/leon/ctf/FracMorse.kt
@@ -1,5 +1,7 @@
 package me.leon.ctf
 
+import me.leon.classical.DEFAULT_MORSE
+
 /**
  *
  * ported from https://programtalk.com/vs2/?source=python/5968/pycipher/pycipher/fracmorse.py
@@ -7,64 +9,6 @@ package me.leon.ctf
  * @since 2022-10-21 15:07
  * @email: deadogone@gmail.com
  */
-val DEFAULT_MORSE =
-    mapOf(
-        'A' to ".-",
-        'B' to "-...",
-        'C' to "-.-.",
-        'D' to "-..",
-        'E' to ".",
-        'F' to "..-.",
-        'G' to "--.",
-        'H' to "....",
-        'I' to "..",
-        'J' to ".---",
-        'K' to "-.-",
-        'L' to ".-..",
-        'M' to "--",
-        'N' to "-.",
-        'O' to "---",
-        'P' to ".--.",
-        'Q' to "--.-",
-        'R' to ".-.",
-        'S' to "...",
-        'T' to "-",
-        'U' to "..-",
-        'V' to "...-",
-        'W' to ".--",
-        'X' to "-..-",
-        'Y' to "-.--",
-        'Z' to "--..",
-        '1' to ".----",
-        '2' to "..---",
-        '3' to "...--",
-        '4' to "....-",
-        '5' to ".....",
-        '6' to "-....",
-        '7' to "--...",
-        '8' to "---..",
-        '9' to "----.",
-        '0' to "-----",
-        '.' to ".-.-.-",
-        ':' to "---...",
-        ',' to "--..--",
-        ';' to "-.-.-.",
-        '?' to "..--..",
-        '=' to "-...-",
-        '\'' to ".----.",
-        '/' to "-..-.",
-        '!' to "-.-.--",
-        '-' to "-....-",
-        '+' to ".-.-.",
-        '_' to "..--.-",
-        '"' to ".-..-.",
-        '(' to "-.--.",
-        ')' to "-.--.-",
-        '$' to "...-..-",
-        '&' to ".-...",
-        '@' to ".--.-.",
-    )
-
 val DEFAULT_MORSE_DECODE =
     mutableMapOf<String, Char>().apply { putAll(DEFAULT_MORSE.values.zip(DEFAULT_MORSE.keys)) }
 
