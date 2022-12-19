9750f2ba8ccb9bb132c5732de753be31c2ee0b48
Leon406 <deadogone@gmail.com>
2022-12-19 20:57:35 +0800

    Revert "fix(plugin-apipost): 部分curl解析错误"
    This reverts commit 621876b797cf5ec1b5eb095f8b4139ffb22e720e.

--- plugin-apipost/src/main/kotlin/me/leon/toolsfx/plugin/net/CUrl.kt
+++ plugin-apipost/src/main/kotlin/me/leon/toolsfx/plugin/net/CUrl.kt
@@ -28,7 +28,7 @@ fun String.cookieParse() =
 fun String.parseCurl() =
     trim()
         // 去掉浏览器多余的分割符
-        .replace("""[\^\\]$""".toRegex(), "")
+        .replace("""[\^\\]""".toRegex(), "")
         .split("""\n|\r\n""".toRegex())
         .map { it.trim() }
         .fold(Request(this)) { acc, s ->
