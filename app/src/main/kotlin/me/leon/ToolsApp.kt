491d412dbe10fdb91273e2180fcdaacf449468cf
Leon406 <deadogone@gmail.com>
2022-12-25 16:44:38 +0800

    refactor(app): 删除无用fixSsl操作

--- app/src/main/kotlin/me/leon/ToolsApp.kt
+++ app/src/main/kotlin/me/leon/ToolsApp.kt
@@ -1,12 +1,9 @@
 package me.leon
 
 import java.io.File
-import java.security.SecureRandom
-import java.security.cert.X509Certificate
 import java.util.Locale
 import java.util.Properties
 import javafx.scene.image.Image
-import javax.net.ssl.*
 import me.leon.ext.fx.Prefs
 import me.leon.view.Home
 import tornadofx.*
@@ -17,12 +14,11 @@ class ToolsApp : App(Home::class, Styles::class) {
     }
 
     companion object {
-        val properties: Properties = Properties()
+        private val properties: Properties = Properties()
         init {
             // for text i18n
             FX.locale = if (Prefs.language == "zh") Locale.CHINESE else Locale.ENGLISH
             initConfig()
-            fixSsl()
         }
         val isEnableClassical: Boolean
             get() = properties["isEnableClassical"].toString().toBoolean()
@@ -61,35 +57,6 @@ class ToolsApp : App(Home::class, Styles::class) {
             file.parentFile.mkdirs()
 
             javaClass.getResourceAsStream("/top1000.txt")?.use { it.copyTo(file.outputStream()) }
-        }
-
-        private fun fixSsl() {
-            val trustManagers =
-                arrayOf(
-                    object : X509TrustManager {
-                        override fun checkClientTrusted(
-                            chain: Array<out X509Certificate>?,
-                            authType: String?
-                        ) {
-                            // nop
-                        }
-
-                        override fun checkServerTrusted(
-                            chain: Array<out X509Certificate>?,
-                            authType: String?
-                        ) {
-                            // nop
-                        }
-
-                        override fun getAcceptedIssuers(): Array<X509Certificate>? {
-                            return null
-                        }
-                    }
-                )
-            val sc = SSLContext.getInstance("TLSv1.2")
-            sc.init(null, trustManagers, SecureRandom())
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.socketFactory)
-            HttpsURLConnection.setDefaultHostnameVerifier { _, _ -> true }
         }
     }
 }
