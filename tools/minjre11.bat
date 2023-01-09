752dbf8a240ea8e66846e3a7d425fd8754cd3636
Leon Lambert <deadogone@gmail.com>
2023-01-09 13:50:59 +0800

    fix(tools): minjre缺少模块

--- tools/minjre11.bat
+++ tools/minjre11.bat
@@ -1,1 +1,1 @@
-%JAVA_HOME_11%/bin/jlink --add-modules java.base,java.desktop,java.logging,jdk.crypto.ec,jdk.unsupported,java.net.http,java.prefs,java.xml,java.datatransfer,jdk.zipfs,jdk.charsets --output jre11 --strip-debug --no-man-pages --no-header-files --compress=2
+%JAVA_HOME_11%/bin/jlink --add-modules java.base,java.desktop,java.logging,jdk.crypto.ec,jdk.unsupported,java.net.http,java.prefs,java.xml,java.datatransfer,jdk.zipfs,jdk.charsets,java.scripting,jdk.dynalink   --output jre11 --strip-debug --no-man-pages --no-header-files --compress=2
--- tools/minjre17.bat
+++ tools/minjre17.bat
@@ -1,1 +1,1 @@
-%JAVA_HOME_17%/bin/jlink --add-modules java.base,java.desktop,java.logging,jdk.crypto.ec,jdk.unsupported,java.net.http,java.prefs,java.xml,java.datatransfer,jdk.zipfs,jdk.charsets --output jre17 --strip-debug --no-man-pages --no-header-files --compress=2
+%JAVA_HOME_17%/bin/jlink --add-modules java.base,java.desktop,java.logging,jdk.crypto.ec,jdk.unsupported,java.net.http,java.prefs,java.xml,java.datatransfer,jdk.zipfs,jdk.charsets,java.scripting,jdk.dynalink --output jre17 --strip-debug --no-man-pages --no-header-files --compress=2
