db198da7877981ffa1b9defca81f6843640b3f8a
Leon Lambert <deadogone@gmail.com>
2023-01-28 18:12:20 +0800

    build: bump jfx_version spotless version

--- config/Versions.gradle.kts
+++ config/Versions.gradle.kts
@@ -1,7 +1,7 @@
 extra.set("kotlin_version", "1.8.0")
 extra.set("tornadofx_version", "1.7.20")
 // openjfx LTS version needs Subscription https://mvnrepository.com/artifact/org.openjfx/javafx-controls
-extra.set("jfx_version", "19")
+extra.set("jfx_version", "19.0.2.1")
 // https://mvnrepository.com/artifact/org.bouncycastle/bcprov-jdk18on  https://www.bouncycastle.org/releasenotes.html
 extra.set("bouncycastle_version", "1.72")
 extra.set("zxing_version", "3.5.1")
