bca05d286bad807c05f3f55423447361e223f9d7
Leon Lambert <deadogone@gmail.com>
2023-04-17 09:03:32 +0800

    build(app): bump bouncy castle to 1.73

--- config/Versions.gradle.kts
+++ config/Versions.gradle.kts
@@ -3,6 +3,6 @@ extra.set("tornadofx_version", "1.7.20")
 // openjfx LTS version needs Subscription https://mvnrepository.com/artifact/org.openjfx/javafx-controls
 extra.set("jfx_version", "19.0.2.1")
 // https://mvnrepository.com/artifact/org.bouncycastle/bcprov-jdk18on  https://www.bouncycastle.org/releasenotes.html
-extra.set("bouncycastle_version", "1.72")
+extra.set("bouncycastle_version", "1.73")
 extra.set("zxing_version", "3.5.1")
 extra.set("javax_json_version", "1.1.4")
