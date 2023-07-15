934b73a636212d985b639843d1a99a0e6906402b
Leon406 <deadogone@gmail.com>
2023-07-15 17:47:33 +0800

    refactor(app): simplify DnsResponse

--- app/src/main/kotlin/me/leon/misc/net/DnsResponse.kt
+++ app/src/main/kotlin/me/leon/misc/net/DnsResponse.kt
@@ -5,18 +5,6 @@ package me.leon.misc.net
  * @since 2023-04-10 9:00
  * @email deadogone@gmail.com
  */
-@Suppress("ALL")
-data class DnsResponse(
-    val Status: Int,
-    val TC: Boolean,
-    val RD: Boolean,
-    val RA: Boolean,
-    val AD: Boolean,
-    val CD: Boolean,
-    val Question: Question,
-    val Answer: List<Answer>
-)
+@Suppress("ALL") data class DnsResponse(val Status: Int, val Answer: List<Answer>)
 
-data class Question(val name: String, val type: Int)
-
-@Suppress("ALL") data class Answer(val name: String, val TTL: Int, val type: Int, val data: String)
+@Suppress("ALL") data class Answer(val name: String, val type: Int, val data: String)
