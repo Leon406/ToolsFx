c35229484418cc5bc4d95e7d04738d4742285bb6
Leon Lambert <deadogone@gmail.com>
2023-11-30 17:22:54 +0800

    feat(app): misc add China car number

--- app/src/main/kotlin/me/leon/misc/CodeMapping.kt
+++ app/src/main/kotlin/me/leon/misc/CodeMapping.kt
@@ -53,10 +53,28 @@ object CodeMapping {
             }
             .toMap()
 
+    /** data from https://zh.wikipedia.org/wiki/%E4%B8%AD%E5%8D%8E%E4%BA%BA%E6%B0%91%E5%85%B1%E5%92%8C%E5%9B%BD%E6%B0%91%E7%94%A8%E6%9C%BA%E5%8A%A8%E8%BD%A6%E5%8F%B7%E7%89%8C */
+    val CN_CAR_NO =
+        readResourceText("/mapping/CNcarNumber.txt")
+            .lines()
+            .filterNot { it.isBlank() }
+            .map { it.split("\t") }
+            .fold(mutableMapOf<String, String>()) { acc, p ->
+                acc.also {
+                    val codes = p.first().split("/")
+                    acc[codes.first()] = p[1]
+                    codes.drop(1).forEach {
+                        acc[codes.first().first() + it] = p[1]
+                    }
+                }
+            }
+            .toMap()
+
     val TYPE =
         mapOf(
             "PORT" to PORT_DICT,
             "HTTP RESPONSE CODE" to HTTP_CODE_DICT,
             "MIME" to MIME_DICT,
+            "China CAR NO" to CN_CAR_NO,
         )
 }
